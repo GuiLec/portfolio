8b934b604f674350fd43a4b8d93b6aadb4b70270
GuiLec <guillaumele@bam.tech>
2024-06-28 15:04:38 +0200

    add description

--- src/app/the-game-of-life/page.tsx
+++ src/app/the-game-of-life/page.tsx
@@ -75,9 +75,30 @@ const TheGameOfLifePage = () => {
   return (
     <main>
       <h1>The Game of Life</h1>
+      <br />
+      <p>
+        The Game of Life is a cellular automaton devised by the British
+        mathematician John Horton Conway in 1970. It is a zero-player game,
+        meaning that its evolution is determined by its initial state, requiring
+        no further input. One interacts with the Game of Life by creating an
+        initial configuration and observing how it evolves.
+      </p>
+      <br />
+      <p>
+        Rules: if a square has 2 or 3 neighbors, it stays alive; if a square has
+        3 neighbors, it becomes alive; otherwise, it dies.
+      </p>
+      <br />
+      <p>
+        {
+          "Click multiple squares to create an initial configuration, then click on 'Start the game' to see how it evolves."
+        }
+      </p>
+      <br />
       <section>
         <button onClick={startGame}>Start the game</button>
       </section>
+      <br />
       <section>
         <div
           style={{
