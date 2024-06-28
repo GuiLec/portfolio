fbcc85cf319cc63971582bbc593190fcda21308f
GuiLec <guillaumele@bam.tech>
2024-06-28 13:15:36 +0200

    Init /the-game-of-life page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,16 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <Link href="/digital-art">Digital art</Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/digital-art">Digital art</Link>
+          </li>
+          <li>
+            <Link href="/the-game-of-life">The Game of Life</Link>
+          </li>
+        </ul>
+      </nav>
     </main>
   );
 }
---
+++ src/app/the-game-of-life/page.tsx
@@ -0,0 +1,7 @@
+export default function TheGameOfLifePage() {
+  return (
+    <main>
+      <h1>The Game of Life</h1>
+    </main>
+  );
+}
