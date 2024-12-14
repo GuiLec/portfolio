fa9d7f8a84cbb551c00d3c2e14fb7814f16536e3
GuiLec <guillaumele@bam.tech>
2024-12-14 20:48:21 +0100

    add dataLayer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,13 +19,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <script>
+        <Script id="gtag-init">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          `}
+        </Script>
+
+        <Script id="gtm-init" strategy="afterInteractive">
           {
             /* <!-- Google Tag Manager --> */
             "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-NCBVQQ4P');"
             /* <!-- End Google Tag Manager --> */
           }
-        </script>
+        </Script>
       </head>
       <body className={inter.className}>
         <noscript>
