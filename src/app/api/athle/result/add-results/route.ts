964d682de9e2d28d0ea47e930100492c3cc90228
GuiLec <guillaumele@bam.tech>
2025-01-31 10:32:26 +0100

    change addResult into addResults

--- src/app/api/athle/result/add-results/route.ts
+++ src/app/api/athle/result/add-results/route.ts
@@ -1,18 +1,19 @@
-import { addResult } from "@/services/athle/result/addResult";
+import { Result } from "@/modules/result/result.type";
+import { addResults } from "@/services/athle/result/addResults";
 import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   const body = await request.json();
-  const { id, fullName, distance, eventDate, eventLocation } = body;
+  const newResults = body.results as Result[];
 
   try {
-    if (!id) throw new Error("id");
-    await addResult({ id, fullName, distance, eventDate, eventLocation });
+    await addResults(newResults);
     const table = await sql`SELECT * FROM AthleResults LIMIT 200;`;
     const results = table.rows;
     return NextResponse.json({ results }, { status: 200 });
   } catch (error) {
+    console.log("🚀 ~ POST ~ error:", error);
     return NextResponse.json({ error }, { status: 500 });
   }
 }
