62bae43ed1150ea013223559401a2bd4d2d46b00
GuiLec <guillaumele@bam.tech>
2025-02-01 22:56:22 +0100

    remove console

--- src/app/athle/components/AthleResultsDataGrid/utils/getFilterParams.ts
+++ src/app/athle/components/AthleResultsDataGrid/utils/getFilterParams.ts
@@ -3,7 +3,6 @@ import { GridFilterModel } from "@mui/x-data-grid";
 export const getFilterParams = (
   filterModel: GridFilterModel
 ): { filterField: string; filterValue: string } => {
-  console.log("🚀 ~ filterModel:", filterModel);
   if (filterModel.items.length === 0) {
     return { filterField: "", filterValue: "" };
   }
