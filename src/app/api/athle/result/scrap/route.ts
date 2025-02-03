ae433c066c2d7fb480eaa3e8ff886791de5bf7cd
GuiLec <guillaumele@bam.tech>
2025-02-03 12:43:52 +0100

    scrap with parameters

--- src/app/api/athle/result/scrap/route.ts
+++ src/app/api/athle/result/scrap/route.ts
@@ -5,8 +5,19 @@ import { NextResponse } from "next/server";
 import puppeteer from "puppeteer";
 import { extractPageResults } from "@/modules/scrapping/extractPageResults";
 
+const EVENT_TYPE_PARAMS_MAPPER = {
+  ["5 Km Route"]: 252,
+  ["10 Km Route"]: 261,
+  ["1/2 Marathon"]: 271,
+  ["Marathon"]: 295,
+};
+
+const MAX_PAGES_TO_SCRAP_BY_SEX_YEAR_AND_EVENT = 100;
+
+const CHUNK_SIZE = 2000;
+
 export async function POST(request: Request) {
-  const { targetUrls } = await request.json();
+  const { scrapRequests } = await request.json();
 
   const results: Result[] = [];
   const browser = await puppeteer.launch();
@@ -14,9 +25,17 @@ export async function POST(request: Request) {
   await page.setViewport({ width: 1080, height: 1024 });
 
   try {
-    for (const targetUrl of targetUrls) {
+    for (const scrapRequest of scrapRequests) {
+      const { year, eventType, sex } = scrapRequest;
+      console.log("🚀 ~ POST ~ year, eventType, sex:", year, eventType, sex);
+      const eventTypeId =
+        EVENT_TYPE_PARAMS_MAPPER[
+          eventType as keyof typeof EVENT_TYPE_PARAMS_MAPPER
+        ];
+
+      const targetUrl = `https://bases.athle.fr/asp.net/liste.aspx?frmpostback=true&frmbase=bilans&frmmode=1&frmespace=0&frmannee=${year}&frmepreuve=${eventTypeId}&frmcategorie=&frmsexe=${sex}&frmnationalite=&frmamini=&frmamaxi=&frmligue=&frmdepartement=&frmclub=&frmvent=&frmathlerama=&frmfcompetition=&frmfepreuve=&frmplaces=`;
+
       const baseUrl = new URL(targetUrl);
-      baseUrl.searchParams.delete("frmposition");
 
       await page.goto(baseUrl.toString());
 
@@ -32,23 +51,27 @@ export async function POST(request: Request) {
       const pageResults = await extractPageResults(page);
       results.push(...pageResults);
 
-      for (let i = 1; i <= numberOfPages - 1; i++) {
+      const numberOfPagesToScrap = Math.min(
+        numberOfPages,
+        MAX_PAGES_TO_SCRAP_BY_SEX_YEAR_AND_EVENT
+      );
+
+      for (let i = 1; i <= numberOfPagesToScrap - 1; i++) {
         console.log("🚀 ~ POST ~ i:", i);
         await page.goto(`${baseUrl.toString()}&frmposition=${i}`);
         // Optional: Add debug screenshot
         // await page.screenshot({ path: `page-${i}.png` });
         const newPageResults = await extractPageResults(page);
         results.push(...newPageResults);
+
+        if (results.length > CHUNK_SIZE) {
+          await addResults(results);
+          console.log("🚀 ~ POST ~ CHUNK SENT !");
+          results.length = 0;
+        }
       }
     }
 
-    // batch results by 2000
-    const chunkSize = 2000;
-    for (let i = 0; i < results.length; i += chunkSize) {
-      console.log("🚀 ~ POST ~ chunk:", i);
-      const chunk = results.slice(i, i + chunkSize);
-      await addResults(chunk);
-    }
     await browser.close();
 
     return NextResponse.json(
